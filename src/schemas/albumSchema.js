afac7b89c4e9e9b449a31eae0af50dffc93e23f6
SergDeikun <92675885+SergDeikun@users.noreply.github.com>
2023-02-17 00:34:45 +0200

    Update albumSchema.js

--- src/schemas/albumSchema.js
+++ src/schemas/albumSchema.js
@@ -2,7 +2,8 @@ const Joi = require("joi");
 
 const albumSchema = Joi.object({
   name: Joi.string().required(),
-  backgroundURL: Joi.string(),
+  // backgroundURL: Joi.string(),
+  files: Joi.any(),
 });
 
 module.exports = albumSchema;
