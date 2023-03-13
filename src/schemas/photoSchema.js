99b44cc0c1d22dae3f8392149ac335670149aec3
SergDeikun <92675885+SergDeikun@users.noreply.github.com>
2023-03-13 13:14:18 +0200

    Update photoSchema.js

--- src/schemas/photoSchema.js
+++ src/schemas/photoSchema.js
@@ -1,10 +1,11 @@
 const Joi = require("joi").extend(require("@joi/date"));
 
 const photoSchema = Joi.object({
-  // place: Joi.string(),
-  // date: Joi.date().format("DD.MM.YYYY").utc(),
-  photoURL: Joi.string().required(),
-  // comments: Joi.string(),
+  place: Joi.string(),
+  date: Joi.date().format("DD.MM.YYYY").utc(),
+  // photoURL: Joi.string().valid("image/jpeg", "image/png").required(),
+  photoURL: Joi.string(),
+  comments: Joi.string(),
 });
 
 module.exports = photoSchema;
