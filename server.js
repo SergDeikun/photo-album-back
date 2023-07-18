c44d580ac33333d0b678299468d2b132389f54d5
SergDeikun <92675885+SergDeikun@users.noreply.github.com>
2023-07-18 17:30:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 const { DB_HOST, PORT = 8080 } = process.env;
+console.log(typeof DB_HOST);
 
 mongoose
+  .set("strictQuery", false)
   .connect(DB_HOST)
   .then(() => {
     console.log(`Server running. Use our API on port: ${PORT}`);
